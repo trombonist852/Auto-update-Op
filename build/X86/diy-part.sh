d2c98f9409febde46ea36b729cf0a846b3c9acf3
trombonist852 <trombonist852@gmail.com>
2021-10-29 19:23:26 +0800

    Add files via upload

--- build/X86/diy-part.sh
+++ build/X86/diy-part.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
@@ -12,6 +13,9 @@
 #Modify Router Name
 sed -i 's/ImmortalWrt/Trb.Corp/g' package/base-files/files/bin/config_generate
 
+#Modify Default Network Interface
+sed -i '/ucidef_set_interface_lan/s/eth0/eth1 eth2 eth3 eth4 eth5/g' package/base-files/files/etc/board.d/99-default_network
+sed -i '/ucidef_set_interface_wan/s/eth1/eth0/g' package/base-files/files/etc/board.d/99-default_network
 
 
 #Modify Default Password
@@ -30,17 +34,4 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 
 sed -i "s/OpenWrt /${Author} Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ          # 增加个性名字${Author}默认为你的github账号
-
-mkdir package/base-files/files/etc/modules.d
-svn export https://github.com/maxlicheng/phicomm-n1-wireless/trunk/wireless_enable package/base-files/files/etc/modules.d/
-
-
-
-
-# 设置打包固件的机型，内核组合（请看说明）
-cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
-amlogic_model=s905x3_s905x2_s905x_s905d_s922x_s912
-amlogic_kernel=5.12.12_5.4.127
-rootfs_size=1500
-EOF
 
--- build/k3/diy-part.sh
+++ build/k3/diy-part.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
 
+# K3专用，编译K3的时候只会出K3固件
+sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
 #Modify Router Name
 sed -i 's/ImmortalWrt/Trb.Corp/g' package/base-files/files/bin/config_generate
 
@@ -24,23 +17,10 @@ git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/custom/au
 git clone https://github.com/godros/luci-app-godproxy.git package/custom/luci-app-godproxy
 git clone https://github.com/linkease/nas-packages.git package/custom/nas-packages
 svn checkout https://github.com/trombonist852/custom/trunk/luci-app-filetransfer package/custom/luci-app-filetransfer-mod
+svn export https://github.com/xiangfeidexiaohuo/Phicomm-K3_Wireless-Firmware/trunk/brcmfmac4366c-pcie.bin_69027 package/firmware/brcmfmac4366c0-firmware-vendor/files/brcmfmac4366c-pcie.bin
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile           # 选择argon为默认主题
 
 
 
 sed -i "s/OpenWrt /${Author} Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ          # 增加个性名字${Author}默认为你的github账号
-
-mkdir package/base-files/files/etc/modules.d
-svn export https://github.com/maxlicheng/phicomm-n1-wireless/trunk/wireless_enable package/base-files/files/etc/modules.d/
-
-
-
-
-# 设置打包固件的机型，内核组合（请看说明）
-cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
-amlogic_model=s905x3_s905x2_s905x_s905d_s922x_s912
-amlogic_kernel=5.12.12_5.4.127
-rootfs_size=1500
-EOF
-
