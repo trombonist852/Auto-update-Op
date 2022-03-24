191abc6104b0bdd9c8b14eea2052d939dadfb932
trombonist852 <trombonist852@gmail.com>
2022-03-24 22:19:35 +0800

    Update diy-part.sh

--- build/X86/diy-part.sh
+++ build/X86/diy-part.sh
@@ -12,6 +12,7 @@
 
 #Modify Router Name
 sed -i 's/ImmortalWrt/Trb.Corp/g' package/base-files/files/bin/config_generate
+sed -i 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files
 
 #Modify Default Network Interface
 sed -i '/ucidef_set_interface_lan/s/eth0/eth1 eth2 eth3/g' package/base-files/files/etc/board.d/99-default_network
